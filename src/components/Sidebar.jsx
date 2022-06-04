9f005ac4970d118d34a119a4cfba608c66bd7af4
chathu_08 <chathu.rash97@gmail.com>
2022-06-04 08:37:08 +0530

    Implemented the navbar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,7 +11,13 @@ import { useStateContext } from '../context/ContextProvider'
 
 const Sidebar = () => {
 
-    const { activeMenu, setactiveMenu } = useStateContext()
+    const { activeMenu, setactiveMenu, screenSize } = useStateContext()
+
+    const handleCloseSidebar = () => {
+        if (activeMenu && screenSize <= 900) {
+            setactiveMenu(false)
+        }
+    }
 
     const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-md m-2 text-white' 
     const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2' 
@@ -23,8 +29,8 @@ const Sidebar = () => {
                     <div className='flex justify-between items-center'>
                         <Link 
                             to="/" 
-                            onClick={() => setactiveMenu(false)}
-                            className='gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'
+                            onClick={handleCloseSidebar}
+                            className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'
                         >
                             <SiShopware /> <span>Shoppy</span>
                         </Link>
@@ -54,7 +60,7 @@ const Sidebar = () => {
                                     <NavLink
                                         to={`/${link.name}`}
                                         key={link.name}
-                                        onClick={() => {}}
+                                        onClick={handleCloseSidebar}
                                         className={({ isActive }) => ( isActive ? activeLink : normalLink )}
                                     >
                                         { link.icon }
--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useState } from "react";
 const StateContext = createContext()
 
 const initialState = {
-    chart: false,
+    chat: false,
     cart: false,
     userProfile: false,
     notification: false,
@@ -12,12 +12,23 @@ const initialState = {
 export const ContextProvider = ({ children }) => {
 
     const [activeMenu, setactiveMenu] = useState(true)
+    const [isClicked, setisClicked] = useState(initialState)
+    const [screenSize, setscreenSize] = useState(undefined)
+
+    const handleClick = (clicked) => {
+        setisClicked({ ...initialState, [clicked]: true })
+    }
 
     return (
         <StateContext.Provider
             value={{ 
                 activeMenu,
                 setactiveMenu,
+                isClicked,
+                setisClicked,
+                handleClick,
+                screenSize, 
+                setscreenSize
              }}
         >
             { children }
